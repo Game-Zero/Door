ac3131b44238ef61b48bff3ab01d5c00802790fe
Zero <1018921287@qq.com>
2024-10-30 00:41:44 +0800

    【feature】2-1动画播放调整

--- Runtime/Script/Manager/PlayerManager_4_1.gd
+++ Runtime/Script/Manager/PlayerManager_4_1.gd
@@ -1,38 +1,75 @@
+@tool
 extends CharacterBody2D
 
-@export var speed = 300.0
+@export var speed: float = 300.0
 
 enum PersonAnimationState {
 	Standing,
 	Walking,
+	Lie,
+	LieCycle,
+	LieOpenBelly,
+	LieSuegeryEnd,
+}
+
+enum PersonState {
+	Medium,
+	Thin,
 }
 
 @onready var animation_player = $AnimationPlayer
-@export var person_animation_state: PersonAnimationState
+@export var person_animation_state: PersonAnimationState = PersonAnimationState.Standing
+@export var person_state: PersonState = PersonState.Medium
 @export var camera: Camera2D
+@export var b_can_move: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	b_can_move = true
 	pass
+	
+func set_can_move(can_move: bool) -> bool:
+	self.b_can_move = can_move
+	return self.b_can_move
+
+func get_can_move() -> bool:
+	return self.b_can_move
 
 func play_animation():
-	var animation_name = "protagonist_thinner_"
+	var animation_name: String = "protagonist_"
+	match (person_state):
+		PersonState.Medium:
+			animation_name += "medium_"
+		PersonState.Thin:
+			animation_name += "thin_"
+
 	match (person_animation_state):
 		PersonAnimationState.Standing:
 			animation_name += "standing"
 		PersonAnimationState.Walking:
 			animation_name += "walking"
+		PersonAnimationState.Lie:
+			animation_name += "lie"
+		PersonAnimationState.LieCycle:
+			animation_name += "lie_cycle"
+		PersonAnimationState.LieOpenBelly:
+			animation_name += "lie_open_belly"
+		PersonAnimationState.LieSuegeryEnd:
+			animation_name += "lie_suegery_end"
 
 	if animation_player != null and animation_player.has_animation(animation_name):
 		animation_player.play(animation_name)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta) -> void:
 	play_animation()
 	if Engine.is_editor_hint() or !get_parent().visible:
 		return
 
-	var direction = Input.get_axis("player_left", "player_right")
+	if (not self.b_can_move):
+		return
+
+	var direction: float = Input.get_axis("player_left", "player_right")
 	if direction:
 		velocity.x = direction * speed
 		person_animation_state = PersonAnimationState.Walking
@@ -48,7 +85,7 @@ func _process(delta):
 
 	move_and_slide()
 
-	var x = global_position.x
+	var x: float = global_position.x
 	if camera and 960 <= x and x <= 2115:
 		#var tween: Tween = get_tree().create_tween()
 		#tween.set_trans(Tween.TRANS_QUAD) # warning-ignore:return_value_discarded
--- Runtime/Script/Manager/Stage_2_1.gd
+++ Runtime/Script/Manager/Stage_2_1.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var bHasSurgeryDone: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +9,20 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func on_doll_body_entered(body: Node2D) -> void:
+	print("[Stage_2_1][on_doll_body_entered] body.name: ", body.name)
+	if (self.bHasSurgeryDone):
+		return
+	
+	self.bHasSurgeryDone = true
+	body.set_can_move(false)
+	body.person_state = body.PersonState.Medium
+	body.person_animation_state = body.PersonAnimationState.Lie
+	body.set_global_position(Vector2(1280, 570))
+
+
+func on_door_body_entered(body: Node2D) -> void:
+	print("[Stage_2_1][on_door_body_entered] body.name: ", body.name)
+	
