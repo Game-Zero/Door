775bc457c4a261ea31ff4acaaff1f396fecb058b
Zero <1018921287@qq.com>
2024-10-30 23:20:28 +0800

    【feature】1-1灯光颜色逻辑调整

--- Runtime/Script/Manager/s_1_1_light.gd
+++ Runtime/Script/Manager/s_1_1_light.gd
@@ -1,6 +1,9 @@
 extends Sprite2D
 
 @onready var light = $Polygon2D
+@export var NormalLightColor: Color = Color(0, 1, 0, 0.3)
+@export var FailedLightColor: Color = Color(1, 0, 0, 0.3)
+
 var b_player_colliding = false
 var player = null
 
@@ -12,9 +15,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (player && player.b_is_game_over):
-		light.color = Color(1, 0, 0, 0.3)
+		light.color = FailedLightColor
 	else:
-		light.color = Color(0, 1, 0, 0.3)
+		light.color = NormalLightColor
 	pass
 
 func _on_body_entered(player):
